2f7280ffa38080617ee1e25b4ad459707b08f716
kraell <kraell@umich.edu>
2021-04-15 14:45:02 -0400

    First push from local dev with outfitted website ready to deploy

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 import { Component } from 'react';
+// import ReactGA from 'react-ga';
+import $ from 'jquery';
+import Header from './Components/Header';
+import About from './Components/About';
+import Resume from './Components/Resume';
+import Portfolio from './Components/Portfolio';
+import Testimonials from './Components/Testimonials';
+import Contact from './Components/Contact';
+import Footer from './Components/Footer';
 
 // function App() {
 class App extends Component {
@@ -9,22 +17,43 @@ class App extends Component {
     super(props);
     this.state = {
       foo: 'bar',
-      data: {}
-    }
+      resumeData: {}
+    };
+
+    // No google analytics necessary right now
+    // ReactGA.initialize('UA-110570651-1');
+    // ReactGA.pageview(window.location.pathname);
+  }
+
+  getResumeData(){
+    $.ajax({
+      url:'/resumeData.json',
+      dataType:'json',
+      cache: false,
+      success: function(data){
+        this.setState({resumeData: data});
+      }.bind(this),
+      error: function(xhr, status, err){
+        console.log(err);
+        alert(err);
+      }
+    });
+  }
+
+  componentDidMount(){
+    this.getResumeData();
   }
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          {/* <img src={logo} className="App-logo" alt="logo" /> */}
-          <p>
-            <code>Hi, I'm Keegan Elliott. Welcome to my website.</code>
-            <br />
-            <br />
-            <code>Things are a bit messy at the moment, but the site should be back up and running in no time. Thanks for bearing with me!</code>
-          </p>
-        </header>
+        <Header data={this.state.resumeData.main}/>
+        <About data={this.state.resumeData.main}/>
+        <Resume data={this.state.resumeData.resume}/>
+        <Portfolio data={this.state.resumeData.portfolio}/>
+        <Testimonials data={this.state.resumeData.testimonials}/>
+        {/* <Contact data={this.state.resumeData.main}/> */}
+        <Footer data={this.state.resumeData.main}/>
       </div>
     );
   }
